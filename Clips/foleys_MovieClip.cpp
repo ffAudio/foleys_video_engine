b51feeaee59bd20dfa5c0a0bfb6c3f72ca4a48fe
Daniel Walz <developer@foleysfinest.com>
2020-12-12 14:11:32 +0100

    Another attempt to render OpenGL frames

--- Clips/foleys_MovieClip.cpp
+++ Clips/foleys_MovieClip.cpp
@@ -120,11 +120,52 @@ VideoFrame& MovieClip::getFrame (double pts)
 void MovieClip::render (double pts)
 {
     auto& frame = getFrame (pts);
+    if (frame.image.isNull())
+        return;
+
     if (! frame.upToDate)
+    {
         frame.texture.loadImage (frame.image);
+        frame.upToDate = true;
+    }
 
     glEnable(GL_TEXTURE_2D);
     frame.texture.bind();
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+    static const GLfloat g_vertex_buffer_data[] = {
+       -1.0f, -1.0f, 0.0f,
+        1.0f, -1.0f, 0.0f,
+        1.0f,  1.0f, 0.0f,
+       -1.0f,  1.0f, 0.0f,
+    };
+    // This will identify our vertex buffer
+    GLuint vertexbuffer;
+    // Generate 1 buffer, put the resulting identifier in vertexbuffer
+    glGenBuffers (1, &vertexbuffer);
+    // The following commands will talk about our 'vertexbuffer' buffer
+    glBindBuffer (GL_ARRAY_BUFFER, vertexbuffer);
+    // Give our vertices to OpenGL.
+    glBufferData (GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+    // 1st attribute buffer : vertices
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glVertexAttribPointer(
+       0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+       4,                  // size
+       GL_FLOAT,           // type
+       GL_FALSE,           // normalized?
+       0,                  // stride
+       nullptr             // array buffer offset
+    );
+    
+    // Draw the rectangle !
+    glDrawArrays (GL_TRIANGLES, 0, 4); // Starting from vertex 0; 3 vertices total -> 1 triangle
+    glDisableVertexAttribArray (0);
+
 
     // Draw a textured quad - FIXME: need a replacement for current OpenGL
 //    glBegin (GL_QUADS);
