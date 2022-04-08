478810c7cd8778b818063957cc1b08d0aeceee4d
Dylan Marcus <dylan@dylanjmarcus.com>
2022-04-08 16:46:06 -0400

    updated macos ffmpeg setup script to use homebrew entirely on stable ffmpeg version

--- scripts/build-ffmpeg-osx.sh
+++ scripts/build-ffmpeg-osx.sh
@@ -2,33 +2,31 @@
 
 set -e
 
-if [[ $# -ne 2 ]]; then
-    echo "Please add the path to the ffmpeg repository as first argument"
-    exit 1
+if [ ! `which brew` ]
+then
+    echo 'Homebrew not found. Trying to install...'
+                ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" \
+        || exit 1
 fi
-
-mkdir -p ../libs
-pushd ../libs
-mkdir -p ffmpeg.build
-pushd ffmpeg.build
 
 if [ ! `which yasm` ]
 then
     echo 'Yasm not found'
-    if [ ! `which brew` ]
-    then
-        echo 'Homebrew not found. Trying to install...'
-                    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" \
-            || exit 1
-    fi
     echo 'Trying to install Yasm...'
     brew install yasm || exit 1
 fi
 
-# ../ffmpeg/configure --enable-shared --disable-static --enable-gpl --enable-libx264 --install-name-dir='@executable_path/../Resources/ffmpeg_libs' --prefix=`pwd` --libdir=`pwd`/ffmpeg_libs
-../$1/configure --enable-shared --disable-static --install-name-dir='@executable_path/../Resources/x86_64' --libdir=`pwd`/../MacOSX/x86_64 --prefix=`pwd` --incdir=`pwd`/../include
-make -j 8
-make install
+echo 'Removing old tap'
+brew uninstall ffmpeg@4.3
+brew untap $USER/ffmpeg-4-3
 
-popd
-popd
+echo 'Installing FFmpeg 4.3.2_4'
+# TODO: Add error handling for this install (will fail if already exists)
+brew tap-new $USER/ffmpeg-4-3
+brew extract --version=4.3 ffmpeg $USER/ffmpeg-4-3
+brew install $USER/ffmpeg-4-3/ffmpeg@4.3
+
+echo 'Copying include directory to module'
+rsync -rl /usr/local/Cellar/ffmpeg@4.3/4.3.2_4/include/ ../ffmpeg/include
+echo 'Copying macOS libs to module'
+rsync -rl /usr/local/Cellar/ffmpeg@4.3/4.3.2_4/lib/ ../libs/MacOSX/x86_64
