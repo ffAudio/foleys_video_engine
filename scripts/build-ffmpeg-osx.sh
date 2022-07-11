7bb79943bb85783de5dc5b273cc5c1498123d9b0
Dylan Marcus <dylan@mach1studios.com>
2022-07-11 16:42:46 -0400

    small improvements to script for safer usage

--- scripts/build-ffmpeg-osx.sh
+++ scripts/build-ffmpeg-osx.sh
@@ -15,16 +15,17 @@ then
 fi
 
 echo 'Removing old tap'
-brew uninstall ffmpeg@4.3
-brew untap $USER/ffmpeg-4-3
+brew uninstall ffmpeg@4.4
+brew untap $USER/ffmpeg-4-4
 
-echo 'Installing FFmpeg 4.3.2_4'
-# TODO: Add error handling for this install (will fail if already exists)
-brew tap-new $USER/ffmpeg-4-3
-brew extract --version=4.3 ffmpeg $USER/ffmpeg-4-3
-brew install $USER/ffmpeg-4-3/ffmpeg@4.3
+echo 'Installing FFmpeg 4.4'
+brew tap-new $USER/ffmpeg-4-4
+brew extract --version=4.4 ffmpeg $USER/ffmpeg-4-4
+brew install $USER/ffmpeg-4-4/ffmpeg@4.4
+# unlinking this flavor of ffmpeg so its not used elsewhere and is just being used to download and locally compile this specific version
+brew unlink $USER/ffmpeg-4-4/ffmpeg@4.4
 
 echo 'Copying include directory to module'
-rsync -rl /usr/local/Cellar/ffmpeg@4.3/4.3.2_4/include/ ../ffmpeg/include
+rsync -rl /usr/local/Cellar/ffmpeg@4.4/*/include/ ../ffmpeg/include
 echo 'Copying macOS libs to module'
-rsync -rl /usr/local/Cellar/ffmpeg@4.3/4.3.2_4/lib/ ../libs/MacOSX/x86_64
+rsync -rl /usr/local/Cellar/ffmpeg@4.4/*/lib/ ../libs/MacOSX/x86_64
